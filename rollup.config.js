dc4a2c3d0365603a8feaa97b0d2e13eff1343cf2
Ben Winding <hello@benwinding.com>
2020-05-06 22:51:26 +0930

    More obscure dev port

--- rollup.config.js
+++ rollup.config.js
@@ -61,7 +61,7 @@ function serve() {
 			if (!started) {
 				started = true;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				require('child_process').spawn('npm', ['run', 'start', '--', '--dev', '--port', '5005'], {
 					stdio: ['ignore', 'inherit', 'inherit'],
 					shell: true
 				});
