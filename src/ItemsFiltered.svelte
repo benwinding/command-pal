e2fd8fd32a02484576f4475ab620a578a8ef2ebc
Ben Winding <hello@benwinding.com>
2020-05-09 01:08:06 +0930

    Shortcuts showing now too

--- src/ItemsFiltered.svelte
+++ src/ItemsFiltered.svelte
@@ -6,32 +6,51 @@
 
   function clickedIndex(e, hoverIndex) {
     const isPrimaryButton = e.which === 1;
-    if (!isPrimaryButton) { 
+    if (!isPrimaryButton) {
       return;
     }
-    dispatch('clickedIndex', hoverIndex);
+    dispatch("clickedIndex", hoverIndex);
   }
 </script>
 
 <style>
-  p {
+  .item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     color: white;
     margin: 0px;
     padding: 7px;
   }
-  p:hover {
+  .item:hover {
     background-color: rgb(24, 94, 173);
     cursor: pointer;
   }
   .selected {
     background-color: rgb(26, 77, 134);
   }
+  kyb {
+    background: #353535;
+    color: #e2e2e2;
+    padding: 1px 4px;
+    border-radius: 6px;
+    border-bottom: 2px solid #7b7b7b;
+    font-family: monospace;
+  }
 </style>
 
 <div>
   {#each items as item, index}
-    <p class:selected={index == selectedIndex} on:mousedown={e => clickedIndex(e, index)}>
-      {item.name}
+    <p
+      class="item"
+      class:selected={index == selectedIndex}
+      on:mousedown={e => clickedIndex(e, index)}>
+      <span>{item.name}</span>
+      {#if !!item.shortcut}
+        <kyb>{item.shortcut}</kyb>
+      {:else}
+        <span />
+      {/if}
     </p>
   {/each}
 </div>
